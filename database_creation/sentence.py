ebdefeffbb55f50b85628125685246783d2dec29
clementjumel <clement.jumel@polytechnique.edu>
2019-07-22 18:00:22 -0400

    Update sentence.py

--- database_creation/sentence.py
+++ database_creation/sentence.py
@@ -1,4 +1,4 @@
-from database_creation.utils import BaseClass, Dependency
+from database_creation.utils import BaseClass
 from database_creation.np import Np
 from database_creation.token import Token
 
@@ -8,7 +8,7 @@ from nltk import tree
 class Sentence(BaseClass):
     # region Class initialization
 
-    to_print = ['tokens', 'text', 'nps', 'dependencies']
+    to_print = ['text', 'nps']
     print_attribute, print_lines, print_offsets = False, 1, 2
 
     def __init__(self, sentence_element):
@@ -21,15 +21,11 @@ class Sentence(BaseClass):
 
         self.tokens = None
         self.parse = None
-        self.dependencies = None
-
         self.text = None
         self.nps = None
 
         self.compute_tokens(sentence_element)
         self.compute_parse(sentence_element)
-        self.compute_dependencies(sentence_element)
-
         self.compute_text()
         self.compute_nps()
 
@@ -61,42 +57,6 @@ class Sentence(BaseClass):
         """
 
         self.parse = sentence_element.find('./parse').text
-
-    def compute_dependencies(self, sentence_element):
-        """
-        Compute the dependencies of the sentence.
-
-        Args:
-            sentence_element: ElementTree.Element, annotations of the sentence.
-        """
-
-        dependencies = {}
-
-        for dependency_element in sentence_element.findall('./dependencies'):
-            dependency_list = []
-
-            for dep in dependency_element:
-                type_ = dep.attrib['type']
-
-                gov_idx = int(dep.find('governor').attrib['idx'])
-                dep_idx = int(dep.find('dependent').attrib['idx'])
-
-                if gov_idx != 0:
-                    gov_word = self.tokens[gov_idx].word
-                    assert gov_word == dep.find('governor').text
-                else:
-                    gov_word = 'ROOT'
-
-                dep_word = self.tokens[dep_idx].word
-                assert dep_word == dep.find('dependent').text
-
-                dependency_list.append(Dependency(type_=type_,
-                                                  gov_word=gov_word, gov_idx=gov_idx,
-                                                  dep_word=dep_word, dep_idx=dep_idx))
-
-            dependencies[dependency_element.attrib['type']] = dependency_list
-
-        self.dependencies = dependencies
 
     def compute_text(self):
         """ Compute the text defined by the tokens. """
@@ -145,17 +105,6 @@ class Sentence(BaseClass):
 
         self.nps = nps
 
-    def compute_similarities(self, entities):
-        """
-        Compute the similarities of the NPs to the entities in the article.
-
-        Args:
-            entities: dict, full entities of the sentence.
-        """
-
-        for np in self.nps:
-            np.compute_similarities(entities)
-
     # endregion
 
 
@@ -165,14 +114,10 @@ def main():
     tree = ElementTree.parse('../databases/nyt_jingyun/content_annotated/2000content_annotated/1165027.txt.xml')
     root = tree.getroot()
 
-    entities = {'person': ['James Joyce', 'Richard Bernstein'], 'location': [], 'organization': [],
-                'all': ['James Joyce', 'Richard Bernstein']}
     sentences = [Sentence(sentence_element) for sentence_element in root.findall('./document/sentences/sentence')[:3]]
 
-    for sentence in sentences:
-        sentence.compute_similarities(entities)
-
     print(Sentence.to_string(sentences))
+    return
 
 
 if __name__ == '__main__':
