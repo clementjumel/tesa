1e962518f0059224836a1d9561866ae8f5dd3447
clementjumel <clement.jumel@polytechnique.edu>
2019-07-22 17:48:14 -0400

    Update token.py

--- database_creation/token.py
+++ database_creation/token.py
@@ -1,6 +1,5 @@
-from database_creation.utils import BaseClass, Similarity
+from database_creation.utils import BaseClass
 
-from copy import copy
 from string import punctuation as string_punctuation
 from nltk.corpus import stopwords as nltk_stopwords
 
@@ -8,12 +7,11 @@ from nltk.corpus import stopwords as nltk_stopwords
 class Token(BaseClass):
     # region Class initialization
 
-    to_print = ['word', 'pos', 'ner', 'similarity']
+    to_print = ['word', 'pos', 'ner']
     print_attribute, print_lines, print_offsets = False, 0, 0
 
     punctuation = [p for p in string_punctuation] + ["''", '``']
     stopwords = set(nltk_stopwords.words('english'))
-    embeddings_token = None
 
     def __init__(self, token_element):
         """
@@ -28,8 +26,6 @@ class Token(BaseClass):
         self.pos = None
         self.ner = None
 
-        self.similarity = None
-
         self.compute_annotations(token_element)
 
     def __str__(self):
@@ -41,7 +37,7 @@ class Token(BaseClass):
         """
 
         to_print, print_attribute, print_lines, print_offsets = self.get_parameters()[:4]
-        attributes = copy(to_print) or list(self.__dict__.keys())
+        attributes = to_print or list(self.__dict__.keys())
 
         string1 = self.prefix(print_lines=print_lines, print_offsets=print_offsets) + str(self.word)
         string2 = ''
@@ -77,38 +73,6 @@ class Token(BaseClass):
             self.pos = token_element.find('POS').text
             self.ner = ner if ner != 'O' else None
 
-    def compute_similarity(self, entities):
-        """
-        Compute the similarity of the token to the entities in the article.
-
-        Args:
-            entities: dict, full entities of the sentence.
-        """
-
-        if self.embeddings_token is None:
-            self.load_embeddings(type_='token')
-
-        token = self.find_embedding()
-        if not token:
-            return
-
-        sim = None
-
-        for entity in entities['all']:
-            token_sim = [self.embeddings_token.similarity(token, entity_token) for entity_token in entity.split()
-                         if entity_token in self.embeddings_token.vocab]
-            score = max(token_sim) if token_sim else None
-
-            if score:
-                if sim is None or score > sim.score:
-                    sim = Similarity(score=score, items=[self.word], similar_items=[entity])
-
-                elif sim is not None and sim.score == score:
-                    sim.items.append(self.word)
-                    sim.similar_items.append(entity)
-
-        self.similarity = sim
-
     # endregion
 
     # region Methods criterion_
@@ -143,41 +107,6 @@ class Token(BaseClass):
 
         return True if self.pos == 'POS' else False
 
-    def criterion_exclude(self):
-        """ Check if a token must not be analyzed by similarity methods. """
-
-        if self.criterion_punctuation() or self.criterion_stopwords() \
-                or self.criterion_determiner() or self.criterion_number():
-            return True
-
-    # endregion
-
-    # region Other methods
-
-    def find_embedding(self):
-        """
-        Finds a string that matches the embeddings' vocabulary. Otherwise, returns None.
-
-        Returns:
-            str, token that matches the embedding vocabulary or None.
-        """
-
-        if self.criterion_exclude():
-            return None
-
-        else:
-            if self.word in self.embeddings_token.vocab:
-                return self.word
-
-            if self.word.lower() in self.embeddings_token.vocab:
-                return self.word.lower()
-
-            elif self.lemma.lower() in self.embeddings_token.vocab:
-                return self.lemma.lower()
-
-            else:
-                return None
-
     # endregion
 
 
@@ -187,14 +116,10 @@ def main():
     tree = ElementTree.parse('../databases/nyt_jingyun/content_annotated/2000content_annotated/1165027.txt.xml')
     root = tree.getroot()
 
-    entities = {'person': ['James Joyce', 'Richard Bernstein'], 'location': [], 'organization': [],
-                'all': ['James Joyce', 'Richard Bernstein']}
     tokens = [Token(token_element) for token_element in root.find('./document/sentences/sentence/tokens')]
 
-    for token in tokens:
-        token.compute_similarity(entities)
-
     print(Token.to_string(tokens))
+    return
 
 
 if __name__ == '__main__':
