e92390c14d8a12b30b4596d07d0cefc5290c3c2f
clementjumel <clement.jumel@polytechnique.edu>
2019-07-23 16:30:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from database_creation.database import Database
 
-# region Parameters
 max_size = 10000
 min_articles = 1
 min_queries = 1
-# endregion
 
-# region Pipeline
 database = Database(max_size=max_size, min_articles=min_articles, min_queries=min_queries)
+
 database.preprocess_database()
 database.filter(min_articles=min_articles)
+
 database.preprocess_articles()
 database.filter(min_queries=min_queries)
+
 database.process_wikipedia(load=False)
 database.process_queries(load=False)
-# endregion
