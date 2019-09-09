80d5b249d12040f8ebc0240b3a1400d7102046ad
clementjumel <clement.jumel@polytechnique.edu>
2019-09-09 18:31:43 -0400

    Weekly main.py.

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from database_creation.database import Database
 
 max_size = 10000
+shuffle = True
 min_articles = 1
 min_queries = 1
 
-database = Database(max_size=max_size, min_articles=min_articles, min_queries=min_queries)
+database = Database(max_size=max_size, shuffle=shuffle, min_articles=min_articles, min_queries=min_queries)
 
 database.preprocess_database()
 database.process_articles()
 
-database.process_wikipedia(load=False)
+database.process_wikipedia(load=True)
 database.process_queries(load=False)
+
+database.combine_pkl(in_names=['wikipedia_global', 'wikipedia_size10k_shuffle_articles1_queries1'])
