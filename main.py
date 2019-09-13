f13c3f0b16b1d921b7cb70f5e29cf3eae75a84c3
clementjumel <clement.jumel@polytechnique.edu>
2019-09-13 16:06:09 -0400

    Update main.py.

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ min_queries = 1
 
 database = Database(max_size=max_size, shuffle=shuffle, min_articles=min_articles, min_queries=min_queries)
 
-database.preprocess_database()
-database.process_articles()
+database.preprocess_database(debug=True)
+database.process_articles(debug=True)
 
-database.process_wikipedia(load=False)
-database.process_queries(load=False)
+database.process_wikipedia(load=False, debug=True)
+database.process_queries(load=False, debug=True)
 
 # database.combine_pkl(in_names=[
 #     'wikipedia_global',
