8f016f69f2f4485dc0894af5a49c48c551f229c9
clementjumel <clement.jumel@polytechnique.edu>
2019-10-25 16:36:19 -0400

    Small update.

--- scripts/task_creation.py
+++ scripts/task_creation.py
@@ -11,8 +11,6 @@ database.process_articles(debug=debug)
 database.process_wikipedia(load=True, debug=debug)
 database.process_queries(check_changes=True, debug=debug)
 
-database.combine_wiki()
-
 database.correct_wikipedia(step=0)
 database.correct_wikipedia(step=1)
 database.correct_wikipedia(step=2)
