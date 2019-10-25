4345d112a8935d6e2eb3456ba459bbc58a7cad04
clementjumel <clement.jumel@polytechnique.edu>
2019-10-25 16:35:26 -0400

    Small update.

--- scripts/task_creation.py
+++ scripts/task_creation.py
@@ -1,7 +1,7 @@
 from database_creation.database import Database
 
 max_size = 10000
-debug = False
+debug = True
 
 database = Database(max_size=max_size)
 
@@ -12,3 +12,7 @@ database.process_wikipedia(load=True, debug=debug)
 database.process_queries(check_changes=True, debug=debug)
 
 database.combine_wiki()
+
+database.correct_wikipedia(step=0)
+database.correct_wikipedia(step=1)
+database.correct_wikipedia(step=2)
