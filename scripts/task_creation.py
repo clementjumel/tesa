e5db2fda1843d83f0868b33051dfa4a9c24bd532
clementjumel <clement.jumel@polytechnique.edu>
2019-10-28 18:02:35 -0400

    Small update.

--- scripts/task_creation.py
+++ scripts/task_creation.py
@@ -10,6 +10,6 @@ database.process_articles(debug=debug)
 database.process_wikipedia(load=True, debug=debug)
 
 database.combine_wiki()
-database.correct_wiki()
+database.correct_wiki(out_name='wikipedia_global')
 
 database.process_queries(check_changes=True, debug=debug, csv_seed=1)
