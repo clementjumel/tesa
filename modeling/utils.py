36bc9d777c93d9c68f259fe3e28d2ee5db658910
clementjumel <clement.jumel@polytechnique.edu>
2020-02-06 17:49:50 -0500

    Small update.

--- modeling/utils.py
+++ modeling/utils.py
@@ -87,4 +87,26 @@ def rank(grades):
 
     return rankdata([-grade for grade in grades], method='ordinal')
 
+
+def progression(count, modulo, size, text):
+    """
+    Prints progression's updates and update the count.
+
+    Args:
+        count: int, current count.
+        modulo: int, how often to print updates.
+        size: int, size of the element to count.
+        text: str, what to print at the beginning of the updates.
+
+    Returns:
+        int, incremented count of articles.
+    """
+
+    count += 1
+
+    if count % modulo == 0:
+        print("   " + text + " {}/{}...".format(count, size))
+
+    return count
+
 # endregion
