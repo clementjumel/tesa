40f55e15152bef6e0a110ae57cff2fbdaffd9d3e
clementjumel <clement.jumel@polytechnique.edu>
2020-02-17 18:09:15 -0500

    Small update.

--- modeling/pipeline.py
+++ modeling/pipeline.py
@@ -1,6 +1,7 @@
 from database_creation.database import Database
 
-from numpy import split, concatenate, mean
+from numpy import split, concatenate, mean, asarray
+from numpy.random import shuffle
 
 
 class Pipeline:
@@ -16,167 +17,248 @@ class Pipeline:
 
         self.use_k_fold = use_k_fold
 
-        self.raw_data = None
-
-        self.train_set = None
-        self.valid_set = None
-        self.k_fold = None
-
-        self.test_set = None
+        self.train_loader = None
+        self.valid_loader = None
+        self.k_fold_loader = None
+        self.test_loader = None
 
     # endregion
 
     # region Main methods
 
-    def process_data(self, test_proportion, valid_proportion=None, k=None):
+    def process_data(self, batch_size=32, drop_last=True, test_proportion=0.25, valid_proportion=0.25, k=10):
         """
-        Process the pipeline to generate learnable data.
+        Process the pipeline to generate learnable data loaders.
 
         Args:
+            batch_size: int, number of samples in each batch.
+            drop_last: bool, whether or not to delete the last batch if incomplete.
             test_proportion: float, fraction (between 0 and 1) of the data to keep in the test set.
             valid_proportion: float, fraction (between 0 and 1) of the data to keep in the valid set.
             k: int, number of folds to create.
         """
 
-        self.compute_raw_data()
+        raw_data = self.get_raw_data()
 
         if not self.use_k_fold:
-            self.compute_train_valid(valid_proportion=valid_proportion, test_proportion=test_proportion)
+            train_set, valid_set, test_set = self.get_train_valid_split(raw_data=raw_data,
+                                                                        valid_proportion=valid_proportion,
+                                                                        test_proportion=test_proportion)
+
+            self.train_loader = self.get_loader(data=train_set, batch_size=batch_size, drop_last=drop_last)
+            self.valid_loader = valid_set
+            self.test_loader = test_set
 
         else:
-            self.compute_k_fold(k=k, test_proportion=test_proportion)
+            train_sets, valid_sets, test_set, complete_train_set = \
+                self.get_k_fold_split(raw_data=raw_data,
+                                      k=k,
+                                      test_proportion=test_proportion)
 
-    def train_model(self, model):
+            self.k_fold_loader = [(self.get_loader(data=train_sets[i], batch_size=batch_size, drop_last=drop_last),
+                                   valid_sets[i]) for i in range(len(train_sets))]
+
+            self.train_loader = self.get_loader(data=complete_train_set, batch_size=batch_size, drop_last=drop_last)
+            self.valid_loader = []
+            self.test_loader = test_set
+
+    def train_model(self, model, n_epochs=1, n_updates=50):
         """
-        Train a model on the training set.
+        Train a model on the training set and compute the metrics on the validation sets.
 
         Args:
             model: models.Model, model to train.
+            n_epochs: int, number of epochs to perform.
+            n_updates: int, number of batches between the updates.
 
         Returns:
-            train_scores: list, scores of the model during training.
-            valid_scores: list, scores of the model during validation.
+            total_train_losses: np.array, training losses, averaged between the epochs and between the runs.
+            total_valid_losses: np.array, validation losses, averaged between the epochs and between the runs.
+            total_valid_scores: np.array, validation scores, averaged between the epochs and between the runs.
         """
-
-        train_scores, valid_scores = [], []
 
         if not self.use_k_fold:
-            train_scores.extend(model.train(data=self.train_set))
-            valid_scores.extend(model.test(data=self.valid_set))
+            total_train_losses, total_valid_losses, total_valid_scores = model.train(train_loader=self.train_loader,
+                                                                                     valid_loader=self.valid_loader,
+                                                                                     n_epochs=n_epochs,
+                                                                                     n_updates=n_updates)
 
         else:
-            for train_set, valid_set in self.k_fold:
+            total_train_losses, total_valid_losses, total_valid_scores = [], [], []
+
+            for train_loader, valid_loader in self.k_fold_loader:
                 model.reset()
-                train_scores.append(model.train(data=train_set))
-                valid_scores.append(model.test(data=valid_set))
+
+                train_losses, valid_losses, valid_scores = model.train(train_loader=train_loader,
+                                                                       valid_loader=valid_loader,
+                                                                       n_epochs=n_epochs,
+                                                                       n_updates=n_updates)
+                total_train_losses.append(train_losses)
+                total_valid_losses.append(valid_losses)
+                total_valid_scores.append(valid_scores)
+
+            total_train_losses = mean(total_train_losses, axis=0)
+            total_valid_losses = mean(total_valid_losses, axis=0)
+            total_valid_scores = mean(total_valid_scores, axis=0)
 
             model.reset()
-            model.train(data=self.train_set)
+            model.train(train_loader=self.train_loader,
+                        valid_loader=self.valid_loader,
+                        n_epochs=n_epochs,
+                        n_updates=n_updates)
 
-            train_scores, valid_scores = mean(train_scores, axis=0), mean(valid_scores, axis=0)
-            print("Mean score on the k-fold, train: {}; valid: {}".format(mean(train_scores), mean(valid_scores)))
+        return total_train_losses, total_valid_losses, total_valid_scores
 
-        return train_scores, valid_scores
-
-    def test_model(self, model):
+    def test_model(self, model, n_updates=50):
         """
-        Evaluate a model on the test set.
+        Evaluate the model on the test set.
 
         Args:
             model: models.Model, model to test.
+            n_updates: int, number of batches between the updates.
 
         Returns:
-            list, scores of the model during testing.
+            losses: np.array, testing losses averaged between the epochs.
+            scores: np.array, testing scores averaged between the epochs.
         """
 
-        test_scores = model.test(data=self.test_set)
+        losses, scores = model.test(test_loader=self.test_loader, n_updates=n_updates)
 
-        return test_scores
+        return losses, scores
 
     # endregion
 
-    # region Methods compute_
+    # region Methods get_
 
-    def compute_raw_data(self):
-        """ Compute the raw data using the methods from Database. """
-
-        database = Database()
-        self.raw_data = database.process_task(assignment_threshold=5)
-
-        print("Raw data imported ({} samples).".format(self.raw_data.shape[0]))
-
-    def compute_train_valid(self, valid_proportion, test_proportion):
+    @staticmethod
+    def get_raw_data(assignment_threshold=5):
         """
-        Compute the data by splitting the raw data into train, valid and test sets.
+        Returns the raw data using the methods from Database.
 
         Args:
+            int, minimum number a worker has to have worked on the task to be taken into account.
+
+        Returns:
+            2d-array, raw sample from the task, each line corresponding to (inputs, targets)
+        """
+
+        database = Database()
+        raw_data = database.process_task(assignment_threshold=assignment_threshold)
+
+        print("Raw data imported ({} samples).".format(raw_data.shape[0]))
+
+        return raw_data
+
+    @staticmethod
+    def get_train_valid_split(raw_data, valid_proportion, test_proportion):
+        """
+        Returns the data by splitting the raw data into train, valid and test sets.
+
+        Args:
+            raw_data: 2d-array, raw sample from the task, each line corresponding to (inputs, targets).
             valid_proportion: float, fraction (between 0 and 1) of the data to keep in the valid set.
             test_proportion: float, fraction (between 0 and 1) of the data, to keep in the test set.
+
+        Returns:
+            train_set: 2d-array, training samples, each line corresponding to (inputs, targets).
+            valid_set: 2d-array, validation samples, each line corresponding to (inputs, targets).
+            test_set: 2d-array, testing samples, each line corresponding to (inputs, targets).
         """
 
         assert valid_proportion is not None and test_proportion is not None
 
-        n = len(self.raw_data)
+        n = len(raw_data)
         n_valid, n_test = round(valid_proportion * n), round(test_proportion * n)
         n_train = n - n_test - n_valid
 
         assert n_train >= 0 and n_valid >= 0 and n_test >= 0
 
-        train_set, valid_set, test_set = split(self.raw_data, [n_train, n_train + n_valid])
+        train_set, valid_set, test_set = split(raw_data, [n_train, n_train + n_valid])
 
-        train_set = train_set[:, 0], train_set[:, 1]
-        valid_set = valid_set[:, 0], valid_set[:, 1]
-        test_set = test_set[:, 0], test_set[:, 1]
+        print("Split into train ({}, {}%),".format(train_set.shape[0], round(100 * train_set.shape[0] / n)) +
+              " valid ({}, {}%)".format(valid_set.shape[0], round(100 * valid_set.shape[0] / n)) +
+              " and test ({}, {}%) sets.".format(test_set.shape[0], round(100 * test_set.shape[0] / n)))
 
-        self.train_set = train_set
-        self.valid_set = valid_set
-        self.test_set = test_set
+        return train_set, valid_set, test_set
 
-        print("Split into train ({}, {}%),".format(train_set[0].shape[0], round(100 * train_set[0].shape[0] / n)) +
-              " valid ({}, {}%)".format(valid_set[0].shape[0], round(100 * valid_set[0].shape[0] / n)) +
-              " and test ({}, {}%) sets.".format(test_set[0].shape[0], round(100 * test_set[0].shape[0] / n)))
-
-    def compute_k_fold(self, k, test_proportion):
+    @staticmethod
+    def get_k_fold_split(raw_data, k, test_proportion):
         """
-        Compute the data by splitting the raw data into a k-fold cross validation and a test set.
+        Returns the data by splitting the raw data into a k-fold cross validation and a test set.
 
         Args:
+            raw_data: 2d-array, raw sample from the task, each line corresponding to (inputs, targets).
             k: int, number of folds to create.
             test_proportion: float, fraction (between 0 and 1) of the data to keep in the test set.
+
+        Returns:
+            train_sets: list of 2d-arrays, training samples, each line corresponding to (inputs, targets).
+            valid_sets: list of 2d-arrays, validation samples, each line corresponding to (inputs, targets).
+            test_set: 2d-array, testing samples, each line corresponding to (inputs, targets).
+            complete_train_set: 2d-array, all training samples, each line corresponding to (inputs, targets).
         """
 
         assert k is not None and test_proportion is not None
 
-        n = len(self.raw_data)
+        n = len(raw_data)
         n_test = round(test_proportion * n)
         n_test += (n - n_test) % k
 
-        test_set, train_set_full = split(self.raw_data, [n_test])
+        test_set, complete_train_set = split(raw_data, [n_test])
 
-        k_splits = split(train_set_full, k)
-        k_fold = []
+        k_splits = split(complete_train_set, k)
+        train_sets, valid_sets = [], []
 
         for i in range(k):
             valid_set = k_splits[i]
             train_set = concatenate([k_splits[j] for j in range(k) if j != i])
 
-            train_set = train_set[:, 0], train_set[:, 1]
-            valid_set = valid_set[:, 0], valid_set[:, 1]
+            train_sets.append(train_set)
+            valid_sets.append(valid_set)
 
-            k_fold.append((train_set, valid_set))
-
-        s = list(set([(train_set[0].shape, valid_set[0].shape) for train_set, valid_set in k_fold]))
+        s = list(set([(train_sets[i].shape, valid_sets[i].shape) for i in range(len(train_sets))]))
         assert len(s) == 1
-
-        train_set_full = train_set_full[:, 0], train_set_full[:, 1]
-        test_set = test_set[:, 0], test_set[:, 1]
-
-        self.k_fold = k_fold
-        self.train_set = train_set_full
-        self.test_set = test_set
 
         print("Split into k-fold cross validation sets (train: {}, {}%,".format(s[0][0][0], round(100*s[0][0][0]/n)) +
               " valid: {}, {}%)".format(s[0][1][0], round(100*s[0][1][0]/n)) +
-              " and a test set ({}, {}%).".format(test_set[0].shape[0], round(100*test_set[0].shape[0]/n)))
+              " and a test set ({}, {}%).".format(test_set.shape[0], round(100*test_set.shape[0]/n)))
+
+        return train_sets, valid_sets, test_set, complete_train_set
+
+    @staticmethod
+    def get_loader(data, batch_size, drop_last):
+        """
+        Returns the loader of the data.
+
+        Args:
+            data: 2d-array, data samples, each line corresponding to (inputs, targets).
+            batch_size: int, number of samples in each batch.
+            drop_last: bool, whether or not to delete the last batch if incomplete.
+
+        Returns:
+            data_loader: 2d-array, batched data samples, each line corresponding to (batch_inputs, batch_targets).
+        """
+
+        data_loader = []
+
+        for inputs, targets in data:
+
+            n = len(inputs['choices'])
+            cmpt = 0
+
+            while (drop_last and cmpt + batch_size <= n) or (not drop_last and cmpt + 1 <= n):
+                batch_inputs = dict([(key, item) for key, item in inputs.items() if key != 'choices'])
+                batch_inputs['choices'] = inputs['choices'][cmpt:cmpt + batch_size]
+
+                batch_targets = targets[cmpt:cmpt + batch_size]
+
+                data_loader.append([batch_inputs, batch_targets])
+
+                cmpt += len(batch_targets)
+
+        shuffle(data_loader)
+        data_loader = asarray(data_loader)
+
+        return data_loader
 
     # endregion
