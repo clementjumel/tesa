e6e7a5dcd525110e63e19053da313dcde04fe460
clementjumel <clement.jumel@polytechnique.edu>
2020-02-27 18:34:33 -0500

    Small update.

--- modeling/nn.py
+++ modeling/nn.py
@@ -90,3 +90,79 @@ class ClassificationMLP(MLP):
 
         self.output_layer = nn.Linear(output_dim, 2)
         self.output_activation = nn.Softmax(dim=1)
+
+
+class Bilinear(nn.Module):
+
+    def __init__(self, input_dim1, input_dim2, output_dim, dropout):
+        """
+        Initialize an instance of Bilinear one layer model.
+
+        Args:
+            input_dim1: int, dimension of the first input.
+            input_dim2: int, dimension of the second input.`
+            output_dim: int, dimension of the output.
+            dropout: float, dropout of the network.
+        """
+
+        super(Bilinear, self).__init__()
+
+        self.dropout = nn.Dropout(dropout)
+
+        self.layer = nn.Bilinear(in1_features=input_dim1, in2_features=input_dim2, out_features=output_dim)
+
+    def forward(self, x1_x2):
+        """
+        Forward pass on the batch of data x.
+
+        Args:
+            x1_x2: tuple, pair of inputs tensor batch of data.
+
+        Returns:
+            torch.tensor, output batch of data.
+        """
+
+        x1, x2 = x1_x2
+
+        x1 = self.dropout(x1)
+        x2 = self.dropout(x2)
+
+        x = self.layer(x1, x2)
+        x = self.activation(x)
+
+        return x
+
+
+class RegressionBilinear(Bilinear):
+
+    def __init__(self, input_dim1, input_dim2, dropout):
+        """
+        Initialize an instance of Bilinear one layer model.
+
+        Args:
+            input_dim1: int, dimension of the first input.
+            input_dim2: int, dimension of the second input.`
+            dropout: float, dropout of the network.
+        """
+
+        super(RegressionBilinear, self).__init__(input_dim1, input_dim2, 1, dropout)
+
+        self.activation = nn.Sigmoid()
+
+
+class ClassificationBilinear(Bilinear):
+
+    def __init__(self, input_dim1, input_dim2, dropout):
+        """
+        Initialize an instance of Bilinear one layer model.
+
+        Args:
+            input_dim1: int, dimension of the first input.
+            input_dim2: int, dimension of the second input.`
+            dropout: float, dropout of the network.
+        """
+
+        super(ClassificationBilinear, self).__init__(input_dim1, input_dim2, 2, dropout)
+
+        self.activation = nn.Softmax(dim=1)
+
