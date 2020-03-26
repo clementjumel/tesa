c5150e5eed333f327e431a385e8d02854edb4dbd
clementjumel <clement.jumel@polytechnique.edu>
2020-03-26 16:48:39 -0400

    Small update.

--- utils.py
+++ utils.py
@@ -14,6 +14,7 @@ def parse_arguments():
     ap.add_argument("-t", "--task_name", required=True, help="Name of the modeling task (e.g. 'ContextFree').")
     ap.add_argument("-m", "--model_name", required=True, help="Name of the baseline (e.g. 'Random').")
     ap.add_argument("-p", "--pretrained_model", help="Name of the pretrained model (e.g. 'word2vec').")
+    ap.add_argument("-e", "--experiment_name", help="Name of the experiment (e.g. 'test_0').")
 
     args = vars(ap.parse_args())
 
