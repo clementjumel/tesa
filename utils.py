62f64b00506d3edb015854f1d7a31370565494c3
clementjumel <clement.jumel@polytechnique.edu>
2020-03-26 15:54:14 -0400

    Small update.

--- utils.py
+++ utils.py
@@ -3,11 +3,10 @@ from argparse import ArgumentParser
 
 def parse_arguments():
     """
-    Use arparse to parse the input arguments and retrieve the task_name and the model_name.
+    Use arparse to parse the input arguments.
 
     Returns:
-        task_name: str, name of the task to use.
-        model_name: str, name of the model to use.
+        dict, arguments passed to the script.
     """
 
     ap = ArgumentParser()
@@ -17,4 +16,4 @@ def parse_arguments():
 
     args = vars(ap.parse_args())
 
-    return args['task_name'], args['model_name']
+    return args
