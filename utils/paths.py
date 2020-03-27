34a863af322f886a710d5ade6a90ada2a98562d4
clementjumel <clement.jumel@polytechnique.edu>
2020-03-27 10:49:09 -0400

    Small update.

---
+++ utils/paths.py
@@ -0,0 +1,11 @@
+"""" Paths of various folders, from the root of the project. """
+
+NYT_ANNOTATED_CORPUS_PATH = "nyt_annotated_corpus/"
+
+ANNOTATION_TASK_RESULTS_PATH = "results/annotations_task/"
+
+MODELING_TASK_FOR_BASELINES_PATH = "results/modeling_task/baselines_split/"
+MODELING_TASK_FOR_MODELS_PATH = "results/modeling_task/models_split/"
+
+BASELINES_RESULTS_PATH = 'results/baselines/'
+MODELS_RESULTS_PATH = 'results/models/'
