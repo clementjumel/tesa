3daf0b5793f47e9d7daf6defd11676603bc71c5a
clementjumel <clement.jumel@polytechnique.edu>
2020-03-27 11:34:20 -0400

    Small update.

--- toolbox/paths.py
+++ toolbox/paths.py
@@ -1,7 +1,6 @@
 """" Paths of various folders, from the root of the project. """
 
 NYT_ANNOTATED_CORPUS_PATH = "nyt_annotated_corpus/"
-
 ANNOTATION_TASK_RESULTS_PATH = "results/annotations_task/"
 
 MODELING_TASK_FOR_BASELINES_PATH = "results/modeling_task/baselines_split/"
