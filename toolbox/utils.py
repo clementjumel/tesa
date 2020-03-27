79c3dd94d26b419fc145e8bc4de2fa6fc9987b03
clementjumel <clement.jumel@polytechnique.edu>
2020-03-27 12:53:05 -0400

    Small update.

--- toolbox/utils.py
+++ toolbox/utils.py
@@ -23,12 +23,13 @@ def parse_arguments():
     return args
 
 
-def get_pretrained_model(pretrained_model_name, folder_path, root=''):
+def get_pretrained_model(args, silent, folder_path, root=''):
     """
-    Returns the pretrained model and its dimension, if relevant.
+    Returns the pretrained model and its dimension, if relevant, given the arguments of the script.
 
     Args:
-        pretrained_model_name: str, name of the pretrained_model.
+        args: dict, arguments passed to the script.
+        silent: bool, silence option.
         folder_path: str, path to the pretrained_models folder.
         root: str, path to the root of the project.
 
@@ -37,20 +38,29 @@ def get_pretrained_model(pretrained_model_name, folder_path, root=''):
         pretrained_model_dim: int, dimension of the pretrained model.
     """
 
-    pretrained_model_names = ["word2vec", "bart_mnli"]
-
-    if pretrained_model_name == pretrained_model_names[0]:
-        fname = root + folder_path + "GoogleNews-vectors-negative300.bin"
-
-        pretrained_model = KeyedVectors.load_word2vec_format(fname=fname, binary=True)
-        pretrained_model_dim = 300
-
-    elif pretrained_model_name == pretrained_model_names[1]:
-        pretrained_model = torch_hub_load("pytorch/fairseq", "bart.large.mnli")
-        pretrained_model_dim = None
+    if "pretrained_model_name" not in args:
+        pretrained_model, pretrained_model_dim = None, None
 
     else:
-        raise Exception("Wrong pretrained model name specified: %s (must be in %s)." % (pretrained_model_name,
-                                                                                        str(pretrained_model_names)))
+        pretrained_model_name = args['pretrained_model_name']
+        pretrained_model_names = ["word2vec", "bart_mnli"]
+
+        if pretrained_model_name == pretrained_model_names[0]:
+            fname = root + folder_path + "GoogleNews-vectors-negative300.bin"
+
+            pretrained_model = KeyedVectors.load_word2vec_format(fname=fname, binary=True)
+            pretrained_model_dim = 300
+
+            print("Word2Vec embedding loaded.") if not silent else None
+
+        elif pretrained_model_name == pretrained_model_names[1]:
+            pretrained_model = torch_hub_load("pytorch/fairseq", "bart.large.mnli")
+            pretrained_model_dim = None
+
+            print("Pretrained BART mnli loaded.") if not silent else None
+
+        else:
+            raise Exception("Wrong pretrained model name: %s (valid names are %s)." % (pretrained_model_name,
+                                                                                       str(pretrained_model_names)))
 
     return pretrained_model, pretrained_model_dim
