7a12aaf83233499254f206299fe1bbaf771ef633
clementjumel <clement.jumel@polytechnique.edu>
2020-03-28 18:53:12 -0400

    Small update.

--- toolbox/utils.py
+++ toolbox/utils.py
@@ -1,26 +1,5 @@
-from argparse import ArgumentParser
 from gensim.models import KeyedVectors
 from torch.hub import load as torch_hub_load
-
-
-def parse_arguments():
-    """
-    Use arparse to parse the input arguments.
-
-    Returns:
-        dict, arguments passed to the script.
-    """
-
-    ap = ArgumentParser()
-
-    ap.add_argument("-t", "--task_name", required=True, help="Name of the modeling task (e.g. 'ContextFree').")
-    ap.add_argument("-m", "--model_name", required=True, help="Name of the baseline (e.g. 'Random').")
-    ap.add_argument("-e", "--experiment_name", help="Name of the experiment (e.g. 'test_0').")
-    ap.add_argument("-p", "--pretrained_model_name", help="Name of the pretrained model (e.g. 'word2vec').")
-
-    args = vars(ap.parse_args())
-
-    return args
 
 
 def get_experiment_name(args, save):
