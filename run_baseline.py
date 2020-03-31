0b8038060e41a14f9d825b7871477b1bd7b60608
clementjumel <clement.jumel@polytechnique.edu>
2020-03-31 13:02:04 -0400

    Small update.

--- run_baseline.py
+++ run_baseline.py
@@ -1,28 +1,44 @@
-from database_creation.modeling_task import ModelingTask
 import modeling.models as models
-from utils import parse_arguments, get_pretrained_model
+from toolbox.utils import to_class_name, load_task, get_pretrained_model
 
-from parameters import \
-    SAVE, VERBOSE, MODELING_TASK_FOR_BASELINES_PATH, \
-    SCORES_NAMES, EXPLAIN_SAMPLES, EXPLAIN_CHOICES
+from argparse import ArgumentParser
+
+from toolbox.parameters import SCORES_NAMES
+
+from toolbox.paths import PRETRAINED_MODELS_PATH, MODELING_TASK_FOR_BASELINES_PATH
 
 
-def play_baseline(task, model, verbose):
+def parse_arguments():
+    """
+    Use arparse to parse the input arguments.
+
+    Returns:
+        dict, arguments passed to the script.
+    """
+
+    ap = ArgumentParser()
+
+    ap.add_argument("-t", "--task", required=True, type=str, help="Name of the modeling task version.")
+    ap.add_argument("-m", "--model", required=True, type=str, help="Name of the model.")
+    ap.add_argument("-p", "--pretrained", type=str, help="Name of the pretrained model, if any.")
+    ap.add_argument("-e", "--experiment", type=str, help="Name of the experiment; if None, don't save.")
+
+    args = vars(ap.parse_args())
+
+    return args
+
+
+def play_baseline(task, model):
     """
     Performs the preview of the data and the evaluation of a baseline.
 
     Args:
         task: modeling_task.ModelingTask, task to evaluate the baseline upon.
         model: models.Baseline, baseline to evaluate.
-        verbose: bool, verbose option.
     """
 
     task.preview_data(model=model, include_train=False, include_valid=True)
     task.valid_model(model=model)
-
-    if verbose:
-        model.display_metrics()
-        task.explain_model(model=model, explain_samples=EXPLAIN_SAMPLES, explain_choices=EXPLAIN_CHOICES)
 
 
 def main():
@@ -30,23 +46,15 @@ def main():
 
     args = parse_arguments()
 
-    task_name = args['task_name']
-    model_name = args['model_name']
+    task_name = args['task']
+    model_name = to_class_name(args['model'])
+    pretrained_model_name = args['pretrained']
+    experiment_name = args['experiment']
 
-    if SAVE and "experiment_name" in args:
-        experiment_name = args['experiment_name']
-    elif SAVE and "experiment_name" not in args:
-        raise Exception("No experiment_name specified, cannot save.")
-    else:
-        experiment_name = None
+    task = load_task(task_name=task_name, folder_path=MODELING_TASK_FOR_BASELINES_PATH)
 
-    if "pretrained_model_name" in args:
-        pretrained_model, pretrained_model_dim = \
-            get_pretrained_model(pretrained_model_name=args['pretrained_model_name'])
-    else:
-        pretrained_model, pretrained_model_dim = None, None
-
-    task = ModelingTask.load_pkl(task_name=task_name, folder_path=MODELING_TASK_FOR_BASELINES_PATH)
+    pretrained_model, pretrained_model_dim = get_pretrained_model(pretrained_model_name=pretrained_model_name,
+                                                                  folder_path=PRETRAINED_MODELS_PATH)
 
     model = getattr(models, model_name)(scores_names=SCORES_NAMES,
                                         relevance_level=task.relevance_level,
@@ -54,7 +62,8 @@ def main():
                                         pretrained_model=pretrained_model,
                                         pretrained_model_dim=pretrained_model_dim)
 
-    play_baseline(task=task, model=model, verbose=VERBOSE)
+    play_baseline(task=task,
+                  model=model)
 
 
 if __name__ == '__main__':
