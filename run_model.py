be6d75e30e72abc1ac10540f91d4b35f25f98fe6
clementjumel <clement.jumel@polytechnique.edu>
2020-03-31 20:38:43 -0400

    Small update.

--- run_model.py
+++ run_model.py
@@ -1,37 +1,46 @@
-from database_creation.modeling_task import ModelingTask
 import modeling.models as models
+from toolbox.utils import to_class_name, load_task, get_pretrained_model
 
-from toolbox.utils import parse_arguments, get_experiment_name, get_pretrained_model
+from argparse import ArgumentParser
 
-# TODO: complete
-from toolbox.parameters import SAVE, SILENT, \
-    SCORES_NAMES, EXPLAIN_EXAMPLES, EXPLAIN_CHOICES
-###
+from toolbox.parameters import SCORES_NAMES, MODELS_RANDOM_SEED
 
 from toolbox.paths import PRETRAINED_MODELS_PATH, MODELING_TASK_FOR_MODELS_PATH
 
 
-def play_model(task, model, silent, explain_examples, explain_choices):
+def parse_arguments():
     """
-    Performs the preview of the data, the training and the validation of a model.
+    Use arparse to parse the input arguments.
+
+    Returns:
+        dict, arguments passed to the script.
+    """
+
+    ap = ArgumentParser()
+
+    ap.add_argument("-t", "--task", required=True, type=str, help="Name of the modeling task version.")
+    ap.add_argument("-m", "--model", required=True, type=str, help="Name of the model.")
+    ap.add_argument("-e", "--experiment", required=True, type=str, help="Name of the experiment.")
+    ap.add_argument("-p", "--pretrained", type=str, help="Name of the pretrained model, if any.")
+    ap.add_argument("--short", action='store_true', help="Shorten modeling task option.")
+
+    args = vars(ap.parse_args())
+
+    return args
+
+
+def play_model(task, model):
+    """
+    Performs the preview of the data, the training and the evaluation of a model.
 
     Args:
-        task: modeling_task.ModelingTask, task to evaluate the model upon.
+        task: modeling_task.ModelingTask, task to evaluate the baseline upon.
         model: models.Model, model to evaluate.
-        silent: bool, silence option.
-        explain_examples: int, number of examples to show in details.
-        explain_choices: int, number of choices to show for each explained example.
     """
 
     task.preview_data(model=model, include_train=True, include_valid=False)
-    # TODO: complete
     task.train_model(model=model)
     task.valid_model(model=model)
-    ###
-
-    if not silent:
-        model.display_metrics()
-        task.explain_model(model=model, explain_examples=explain_examples, explain_choices=explain_choices)
 
 
 def main():
@@ -39,29 +48,29 @@ def main():
 
     args = parse_arguments()
 
-    task_name = args['task_name']
-    model_name = args['model_name']
-    experiment_name = get_experiment_name(args=args, save=SAVE)
-    pretrained_model, pretrained_model_dim = get_pretrained_model(args=args,
-                                                                  silent=SILENT,
+    task_name = args['task']
+    model_name = to_class_name(args['model'])
+    experiment_name = args['experiment']
+    pretrained_model_name = args['pretrained']
+    short = args['short']
+
+    task = load_task(task_name=task_name, folder_path=MODELING_TASK_FOR_MODELS_PATH, short=short)
+
+    pretrained_model, pretrained_model_dim = get_pretrained_model(pretrained_model_name=pretrained_model_name,
                                                                   folder_path=PRETRAINED_MODELS_PATH)
 
-    task = ModelingTask.load_pkl(task_name=task_name, folder_path=MODELING_TASK_FOR_MODELS_PATH)
-
-    # TODO: complete parameters
-    model = getattr(models, model_name)(scores_names=SCORES_NAMES,
+    model = getattr(models, model_name)(sscores_names=SCORES_NAMES,
                                         relevance_level=task.relevance_level,
                                         experiment_name=experiment_name,
                                         pretrained_model=pretrained_model,
-                                        pretrained_model_dim=pretrained_model_dim)
+                                        pretrained_model_dim=pretrained_model_dim,
+                                        random_seed=MODELS_RANDOM_SEED)
 
-    # TODO: complete parameters
     play_model(task=task,
-               model=model,
-               silent=SILENT,
-               explain_examples=EXPLAIN_EXAMPLES,
-               explain_choices=EXPLAIN_CHOICES)
+               model=model)
 
 
 if __name__ == '__main__':
     main()
+
+# TODO: rework
