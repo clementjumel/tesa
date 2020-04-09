37539919134829e8a3624831499b5e00add9b2f1
clementjumel <clement.jumel@polytechnique.edu>
2020-04-09 18:53:56 -0400

    Small update.

--- toolbox/utils.py
+++ toolbox/utils.py
@@ -1,18 +1,48 @@
 from pickle import load
+from gensim.models import KeyedVectors
+from fairseq.models.bart import BARTModel
 
 
 def to_class_name(name):
-    """
-    For a name of the format 'abc_efg', returns the corresponding Class name, of the format 'AbcEfg'.
-
-    Args:
-        name: str, name of the class in format 'abc_efg'.
-
-    Returns:
-        str, name of the class in format 'AbcEfg'.
-    """
+    """ For a name of the format 'abc_efg', returns the corresponding Class name, of the format 'AbcEfg'. """
 
     return "".join([word.capitalize() for word in name.split("_")])
+
+
+def inputs_to_context(inputs):
+    """
+    Returns the context of a ranking task as a string with the wikipedia information followed by the article's context.
+
+    Args:
+        inputs: dict, inputs of a batch.
+    """
+
+    context_elements = []
+
+    for wikipedia in inputs['wikipedia']:
+        if wikipedia != "No information found.":
+            context_elements.append(wikipedia)
+
+    context_elements.append(inputs['context'])
+
+    return " ".join(context_elements)
+
+
+def add_task_argument(ap):
+    """
+    Add to the argument parser the parsing arguments relative to the modeling task.
+
+    Args:
+        ap: argparse.ArgumentParser, argument parser to update with the modeling task relative arguments.
+    """
+
+    ap.add_argument("-t", "--task", required=True, type=str, help="Name of the modeling task version.")
+    ap.add_argument("-vp", "--valid_proportion", default=0.25, type=float, help="Proportion of the validation set.")
+    ap.add_argument("-tp", "--test_proportion", default=0.25, type=float, help="Proportion of the test set.")
+    ap.add_argument("-rs", "--ranking_size", default=None, type=int, help="Size of the ranking tasks.")
+    ap.add_argument("-bs", "--batch_size", default=64, type=int, help="Size of the batches of the task.")
+    ap.add_argument("--cross_validation", action='store_true', help="Cross validation option.")
+    ap.add_argument("--short", action='store_true', help="Shorten modeling task option.")
 
 
 def load_task(args, folder_path):
@@ -57,37 +87,40 @@ def load_task(args, folder_path):
     return task
 
 
-def add_task_argument(ap):
+def get_trained_model(args, folder_path):
     """
-    Add to the argument parser the parsing arguments relative to the modeling task.
+    Returns the trained model (word2vec embedding or bart).
 
     Args:
-        ap: argparse.ArgumentParser, argument parser to update with the modeling task relative arguments.
+        args: dict, arguments passed to the script.
+        folder_path: str, path of the folder to load from (if not in the arguments).
     """
 
-    ap.add_argument("-t", "--task", required=True, type=str, help="Name of the modeling task version.")
-    ap.add_argument("-vp", "--valid_proportion", default=0.25, type=float, help="Proportion of the validation set.")
-    ap.add_argument("-tp", "--test_proportion", default=0.25, type=float, help="Proportion of the test set.")
-    ap.add_argument("-rs", "--ranking_size", default=None, type=int, help="Size of the ranking tasks.")
-    ap.add_argument("-bs", "--batch_size", default=64, type=int, help="Size of the batches of the task.")
-    ap.add_argument("--cross_validation", action='store_true', help="Cross validation option.")
-    ap.add_argument("--short", action='store_true', help="Shorten modeling task option.")
+    use_word2vec = args['word2vec']
+    use_bart = args['bart']
+    folder_path = args['model_path'] or folder_path
+    bart_checkpoint = args['bart_checkpoint']
+    data_path = args['data_path']
 
+    if not use_word2vec and not use_bart:
+        return None
 
-def inputs_to_context(inputs):
-    """
-    Returns the context of a ranking task as a string with the wikipedia information followed by the article's context.
+    elif use_word2vec and not use_bart:
+        fname = folder_path + "GoogleNews-vectors-negative300.bin"
+        word2vec = KeyedVectors.load_word2vec_format(fname=fname, binary=True)
 
-    Args:
-        inputs: dict, inputs of a batch.
-    """
+        print("Word2Vec embedding loaded.\n")
 
-    context_elements = []
+        return word2vec
 
-    for wikipedia in inputs['wikipedia']:
-        if wikipedia != "No information found.":
-            context_elements.append(wikipedia)
+    elif not use_word2vec and use_bart:
+        bart = BARTModel.from_pretrained(model_name_or_path=folder_path,
+                                         checkpoint_file=bart_checkpoint,
+                                         data_name_or_path=data_path)
 
-    context_elements.append(inputs['context'])
+        print("BART loaded.\n")
 
-    return " ".join(context_elements)
+        return bart
+
+    else:
+        raise Exception("Must chose between word2vec and BART.")
