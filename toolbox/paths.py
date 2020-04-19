e79df675a3a85e23e1c61ed52d60ba3f46b18473
clementjumel <clement.jumel@polytechnique.edu>
2020-04-18 21:03:07 -0400

    Small update.

--- toolbox/paths.py
+++ toolbox/paths.py
@@ -10,4 +10,5 @@ ANNOTATION_TASK_RESULTS_PATH = "results/annotation_task/"
 MODELING_TASK_RESULTS_PATH = "results/modeling_task/"
 BASELINES_RESULTS_PATH = 'results/baselines/'
 TENSORBOARD_LOGS_PATH = "results/baselines/tensorboard_logs/"
+FINETUNING_DATA_PATH = "results/finetuning_data/"
 # endregion
