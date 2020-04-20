db3bd63f435eebcd402e34a05c36d0986ea16f29
clementjumel <clement.jumel@polytechnique.edu>
2020-04-19 21:44:14 -0400

    Small update.

--- shell_scripts/preprocess_classification.sh
+++ shell_scripts/preprocess_classification.sh
@@ -6,15 +6,19 @@
 #SBATCH --error=/network/tmp1/jumelcle/errors/preprocess_classification-%j.err
 #SBATCH --output=/network/tmp1/jumelcle/outputs/preprocess_classification-%j.out
 
+FINETUNING_DATA_PATH=/network/tmp1/jumelcle/results/finetuning_data
+PREPROCESSED_DATA_PATH=/network/tmp1/jumelcle/results/preprocessed_data
+TASK=classification
+
 module load miniconda
 source activate nlp
 
-cp -r /network/tmp1/jumelcle/results/finetuning_data/classification/$1 $SLURM_TMPDIR
+cp -r "$FINETUNING_DATA_PATH/$TASK/$1" $SLURM_TMPDIR
 cd $SLURM_TMPDIR
 mv $1 RTE
 
 /network/home/jumelcle/master_thesis/fairseq/examples/roberta/preprocess_GLUE_tasks.sh . RTE
 mv RTE-bin "$1-bin"
 
-rm -r /network/tmp1/jumelcle/results/preprocessed_data/classification/"$1-bin"
-mv "$1-bin" /network/tmp1/jumelcle/results/preprocessed_data/classification/
+rm -r "$PREPROCESSED_DATA_PATH/$TASK/$1-bin"
+mv "$1-bin" "$PREPROCESSED_DATA_PATH/$TASK"
