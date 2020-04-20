746e4b2b90c34ef641e93695f7f04872ae108af7
clementjumel <clement.jumel@polytechnique.edu>
2020-04-19 20:31:08 -0400

    Small update.

--- mt_task.py
+++ mt_task.py
@@ -7,9 +7,8 @@ Usages:
         python mt_task.py -t context_free_same_type -rs 32 -bs 16 -cf v0 -tf v0 --classification --no_save
         python mt_task.py -t context_free_same_type -rs 32 -bs 16 -cf v0 -tf v0 --generation --no_save
     regular usages:
-        python mt_task.py -t context_free_same_type
-        python mt_task.py -t context_free_same_type -rs 32 -bs 16 -cf v0 -tf v0 --classification
-        python mt_task.py -t context_free_same_type -rs 32 -bs 16 -cf v0 -tf v0 --generation
+        python mt_task.py -t context_free_same_type -rs 32 -bs 8 -cf v0 --classification
+        python mt_task.py -t context_free_same_type -rs 32 -bs 8 -cf v0 -tf v0 --generation
 """
 
 import modeling.modeling_task as modeling_task
