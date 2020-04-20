9c1bae18f9a99582ad2aa691536f4da89e013776
clementjumel <clement.jumel@polytechnique.edu>
2020-04-20 18:28:24 -0400

    Small update.

--- shell_scripts/finetune_generation.sh
+++ shell_scripts/finetune_generation.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 #SBATCH --job-name=tune-gen
-#SBATCH --partition=main
+#SBATCH --partition=unkillable
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-gpu=32G
 #SBATCH --time=5:00:00
-#SBATCH --error=/network/tmp1/jumelcle/errors/finetune_generation-%j.err
-#SBATCH --output=/network/tmp1/jumelcle/outputs/finetune_generation-%j.out
+#SBATCH --error=/network/tmp1/jumelcle/logs/finetune_generation-%j.err
+#SBATCH --output=/network/tmp1/jumelcle/logs/finetune_generation-%j.out
 
 PREPROCESSED_DATA_PATH=/network/tmp1/jumelcle/results/preprocessed_data
 MODELS_PATH=/network/tmp1/jumelcle/results/models
 PRETRAINED_MODELS_PATH=/network/tmp1/jumelcle/pretrained_models
 FAIRSEQ_PATH=/network/home/jumelcle/master_thesis/fairseq
 TASK=generation
+
+RESULTS_PATH="$MODELS_PATH/$TASK/$1/$2/$3"
 
 module load miniconda
 source activate nlp
@@ -21,19 +23,28 @@ cp $PRETRAINED_MODELS_PATH/$2.tar.gz $SLURM_TMPDIR
 cd $SLURM_TMPDIR
 tar -xvf $2.tar.gz
 
-rm -r "$MODELS_PATH/$1/$2/$3"
-mkdir -p "$MODELS_PATH/$1/$2/$3/tensorboard_logs"
+rm -r $RESULTS_PATH
+mkdir -p $RESULTS_PATH/tensorboard_logs
 
-TOTAL_NUM_UPDATES=200 # Default: 20000
-WARMUP_UPDATES=5  # Default: 500
+MAX_EPOCHS=5  # Defautl: 10
+MAX_SENTENCES=4  # Default: None
+MAX_TOKENS=1024  # Default: 2048; max length: 946
+UPDATE_FREQ=1
 LR=3e-05
-MAX_TOKENS=1024  # Default: 2048
-UPDATE_FREQ=4
-MAX_EPOCHS=10
+TOTAL_NUM_UPDATES=4470 # Default: 20000
+WARMUP_UPDATES=112  # Default: 500
 
 CUDA_VISIBLE_DEVICES=0,1 python $FAIRSEQ_PATH/train.py $1 \
-    --restore-file $2/model.pt \
+    --max-epoch $MAX_EPOCHS \
+    --max-sentences $MAX_SENTENCES \
     --max-tokens $MAX_TOKENS \
+    --update-freq $UPDATE_FREQ \
+    --lr $LR \
+    --total-num-update $TOTAL_NUM_UPDATES \
+    --warmup-updates $WARMUP_UPDATES \
+    --restore-file $2/model.pt \
+    --save-dir $RESULTS_PATH \
+    --tensorboard-logdir $RESULTS_PATH/tensorboard_logs \
     --task translation \
     --source-lang source \
     --target-lang target \
@@ -55,12 +66,6 @@ CUDA_VISIBLE_DEVICES=0,1 python $FAIRSEQ_PATH/train.py $1 \
     --adam-eps 1e-08 \
     --clip-norm 0.1 \
     --lr-scheduler polynomial_decay \
-    --lr $LR \
-    --total-num-update $TOTAL_NUM_UPDATES \
-    --warmup-updates $WARMUP_UPDATES \
-    --max-epoch $MAX_EPOCHS \
     --skip-invalid-size-inputs-valid-test \
-    --save-dir "$MODELS_PATH/$1/$3" \
-    --tensorboard-logdir "$MODELS_PATH/$1/$3/tensorboard_logs" \
     --find-unused-parameters;
-    # --fp16 --update-freq $UPDATE_FREQ \
+    #--fp16  \
