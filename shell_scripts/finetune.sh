5f82f0f2a1d85ef3f91d28217e8c6586bf45c004
clementjumel <clement.jumel@polytechnique.edu>
2020-05-14 10:41:20 -0400

    Small update.

--- shell_scripts/finetune.sh
+++ shell_scripts/finetune.sh
@@ -3,63 +3,71 @@
 #SBATCH --partition=main
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-gpu=32G
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --error=/network/tmp1/jumelcle/logs/finetune-%j.err
 #SBATCH --output=/network/tmp1/jumelcle/logs/finetune-%j.out
 
+# Recover the scripts arguments
 TASK_TYPE=$1
-MODELING_TASK=$2
-BART=$3
-EXPERIMENT=$4
+TASK=$2
+EXPERIMENT=$3
 
+# Files parameters
+TRAIN_PROPORTION=50
+VALID_PROPORTION=25
+TEST_PROPORTION=25
+RANKING_SIZE=24
+BATCH_SIZE=4
+CONTEXT_FORMAT=v1
+TARGETS_FORMAT=v0
+BART=bart.large.cnn
+
+# Paths
 MASTER_THESIS_PATH=/network/home/jumelcle/master_thesis
-PRETRAINED_MODELS_PATH=/network/tmp1/jumelcle/pretrained_models
 PREPROCESSED_DATA_PATH=/network/tmp1/jumelcle/results/preprocessed_data
-MODELING_TASKS_PATH=/network/tmp1/jumelcle/results/modeling_task
-MODELS_PATH=/network/tmp1/jumelcle/results/models
-RESULTS_PATH="$MODELS_PATH/$TASK_TYPE/$MODELING_TASK/$BART/$EXPERIMENT"
+PRETRAINED_MODELS_PATH=/network/tmp1/jumelcle/pretrained_models
+CHECKPOINTS_PATH=/network/tmp1/jumelcle/results/checkpoints
 
-echo "Parameters:"
-echo $TASK_TYPE
-echo $MODELING_TASK
-echo $BART
-echo $EXPERIMENT
-echo ""
-echo "Results path:"
-echo $RESULTS_PATH
-echo ""
+# Recover full paths/names
+FULL_TASK="$TASK"_"$TRAIN_PROPORTION"-"$VALID_PROPORTION"-"$TEST_PROPORTION"_rs"$RANKING_SIZE"_bs"$BATCH_SIZE"_cf-"$CONTEXT_FORMAT"_tf-"$TARGETS_FORMAT"
+RESULTS_PATH="$CHECKPOINTS_PATH/$TASK_TYPE/$FULL_TASK/$EXPERIMENT"
 
+# Print the parameters
+echo "Parameters:"; echo $TASK_TYPE $TASK $EXPERIMENT; echo
+echo "Results path:"; echo $RESULTS_PATH; echo
+
+# Load miniconda
 module load miniconda
 source activate base
 source activate nlp
 
+# Load pretrained BART
 tar -xf "$PRETRAINED_MODELS_PATH/$BART.tar.gz" -C $SLURM_TMPDIR
-cp -r "$PREPROCESSED_DATA_PATH/$TASK_TYPE/$MODELING_TASK-bin" $SLURM_TMPDIR
-cp "$MODELING_TASKS_PATH/$MODELING_TASK.pkl" $SLURM_TMPDIR
 
+# Load the preprocessed_data
+cp -r "$PREPROCESSED_DATA_PATH/$TASK_TYPE/$FULL_TASK-bin" $SLURM_TMPDIR
+
+# Re-initialize the results folder
 rm -rf $RESULTS_PATH
 mkdir -p $RESULTS_PATH/tensorboard_logs
 
+# Move to SLURM temporary directory
 cd $SLURM_TMPDIR
 
 if [ $TASK_TYPE == "classification" ]
 then
-  MAX_EPOCHS=5  # Defautl: 10
+  # Finetuning parameters
+  MAX_EPOCHS=1  # Defautl: 10
   MAX_SENTENCES=32  # Default: 32
-  MAX_TOKENS=512  # Default: 1024; works: 512
   UPDATE_FREQ=1  # Default: 1
   LR=1e-05
-  TOTAL_NUM_UPDATES=80410  # Default: 1018
+  TOTAL_NUM_UPDATES=48246  # Default: 1018
   WARMUP_UPDATES=4824  # Default: 61
-  SAVE_INTERVAL=1
-  VALID_INTERVAL=1
 
-  CUDA_VISIBLE_DEVICES=0,1 python $MASTER_THESIS_PATH/fairseq/train.py "$MODELING_TASK-bin" \
+  CUDA_VISIBLE_DEVICES=0,1 python $MASTER_THESIS_PATH/fairseq/train.py "$FULL_TASK-bin" \
       --max-epoch $MAX_EPOCHS \
-      --validate-interval $VALID_INTERVAL \
-      --save-interval $SAVE_INTERVAL \
       --max-sentences $MAX_SENTENCES \
-      --max-tokens $MAX_TOKENS \
+      --max-tokens 1024 \
       --update-freq $UPDATE_FREQ \
       --lr $LR \
       --total-num-update $TOTAL_NUM_UPDATES \
@@ -67,6 +75,7 @@ then
       --restore-file $BART/model.pt \
       --save-dir $RESULTS_PATH \
       --tensorboard-logdir $RESULTS_PATH/tensorboard_logs \
+      --no-last-checkpoints \
       --task sentence_prediction \
       --add-prev-output-tokens \
       --layernorm-embedding \
@@ -90,31 +99,36 @@ then
       --lr-scheduler polynomial_decay \
       --best-checkpoint-metric accuracy \
       --maximize-best-checkpoint-metric \
-      --skip-invalid-size-inputs-valid-test \
-      --disable-validation \
+      --memory-efficient-fp16 \
       --find-unused-parameters;
 
 elif [ $TASK_TYPE == "generation" ]
 then
-  TOTAL_NUM_UPDATES=2190
-  WARMUP_UPDATES=100
-  LR=3e-05  # Default: 3e-05
-  MAX_TOKENS=1024  # Default: 2048
+  # Finetuning parameters
+  MAX_EPOCHS=10
+  LR=3e-05
   UPDATE_FREQ=4
-  MAX_EPOCHS=10  # Defautl: None
-  VALID_INTERVAL=1
-  SAVE_INTERVAL=1
-  #MAX_SENTENCES=1  # Default: None, works: 4
+  WARMUP_UPDATES_PERCENT=6
 
-  CUDA_VISIBLE_DEVICES=0,1 python $MASTER_THESIS_PATH/fairseq/train.py "$MODELING_TASK-bin" \
+  if [ $TASK == "context-free-same-type" ]
+  then
+    NUM_UPDATES_PER_EPOCH=249
+  elif [ $TASK == "context-dependent-same-type" ]
+  then
+    NUM_UPDATES_PER_EPOCH=210
+  fi
+
+  TOTAL_NUM_UPDATES=$(($NUM_UPDATES_PER_EPOCH * $MAX_EPOCHS))
+  WARMUP_UPDATES=$(($WARMUP_UPDATES_PERCENT * $TOTAL_NUM_UPDATES / 100))
+
+  # Run the finetunign
+  CUDA_VISIBLE_DEVICES=0,1 python $MASTER_THESIS_PATH/fairseq/train.py "$FULL_TASK-bin" \
       --restore-file $BART/model.pt \
-      --validate-interval $VALID_INTERVAL \
-      --save-interval $SAVE_INTERVAL \
       --no-last-checkpoints \
       --save-dir $RESULTS_PATH \
       --tensorboard-logdir $RESULTS_PATH/tensorboard_logs \
       --max-epoch $MAX_EPOCHS \
-      --max-tokens $MAX_TOKENS \
+      --max-tokens 1024 \
       --update-freq $UPDATE_FREQ \
       --lr-scheduler polynomial_decay \
       --lr $LR \
@@ -127,7 +141,9 @@ then
       --layernorm-embedding \
       --share-all-embeddings \
       --share-decoder-input-output-embed \
-      --reset-optimizer --reset-dataloader --reset-meters \
+      --reset-optimizer \
+      --reset-dataloader \
+      --reset-meters \
       --required-batch-size-multiple 1 \
       --arch bart_large \
       --criterion label_smoothed_cross_entropy \
@@ -139,22 +155,9 @@ then
       --adam-betas "(0.9, 0.999)" \
       --adam-eps 1e-08 \
       --clip-norm 0.1 \
-      --fp16 \
-      --skip-invalid-size-inputs-valid-test \
       --find-unused-parameters;
-      #--max-sentences $MAX_SENTENCES \
-      #--reset-lr-scheduler \
 fi
 
-echo "Done. Evaluating the checkpoints..."
+echo "Done."; echo
 
-rm -r *
-
-sh $MASTER_THESIS_PATH/shell_scripts/evaluate.sh \
-      $TASK_TYPE \
-      $MODELING_TASK \
-      $BART \
-      $EXPERIMENT \
-      $SAVE_INTERVAL \
-      $MAX_EPOCHS \
-      $SAVE_INTERVAL;
+sbatch  ~/master_thesis/shell_scripts/rank.sh $1 $2 $3
