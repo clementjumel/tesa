9d5841d4b6223d9c274eae4fb9d3cd555ba2b79c
clementjumel <clement.jumel@polytechnique.edu>
2020-05-30 22:47:56 -0400

    Small update.

--- modeling/modules.py
+++ modeling/modules.py
@@ -1,11 +1,13 @@
 import torch.nn as nn
 
 
-class LogisticRegression(nn.module):
+class LogisticRegression(nn.Module):
     def __init__(self, input_dim, output_dim):
         super().__init__()
+
         self.layer = nn.Linear(input_dim, output_dim)
-        self.activation = nn.Sigmoid()
+        self.activation = nn.Softmax(dim=1)
 
     def forward(self, x):
         return self.activation(self.layer(x))
+
